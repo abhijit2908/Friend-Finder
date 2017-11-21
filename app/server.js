5a2bdcd1511c1bc846f914488215e27a28be331b
Abhijit <abhijitkghosh29@gmail.com>
2017-11-20 23:54:55 -0500

    did routing

--- app/server.js
+++ app/server.js
@@ -1,10 +1,12 @@
 var express = require ('express');
 var path = require ('path');
-var bodyparser = require ('body-parser');
+var bodyParser = require ('body-parser');
 
 var app = express();
 var PORT = process.env.PORT || 3000;
 
+require("./routing/apiRoutes.js")(app);
+require("./routing/htmlRoutes.js")(app);
 
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
