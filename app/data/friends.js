6cca3c9cf21bee6f78f6aa88d6b97d1fdde2c798
Abhijit <abhijitkghosh29@gmail.com>
2017-12-01 22:35:50 -0500

    final Working app

--- app/data/friends.js
+++ app/data/friends.js
@@ -1,83 +1,101 @@
 
 var friends = [
 {
-	"name":"indira",
-  "photo":"https://i.pinimg.com/736x/9e/6a/78/9e6a786abea31b3b1ba66cd878628910--john-snow-game-of-thrones-jon-schnee.jpg",
+	"name":"Darth Vader",
+  "photo":"http://www.globalo.com/content/uploads/2015/12/darth-vader.jpg",
   "scores":[
-      5,
       1,
-      4,
-      4,
-      5,
       1,
-      2,
-      5,
-      4,
+      1,
+      1,
+      1,
+      1,
+      1,
       1
     ]
 },
 {
-	"name":"Ravi",
-  "photo":"https://upload.wikimedia.org/wikipedia/en/9/94/Cersei_Lannister-Lena_Headey.jpg",
+	"name":"Lord Voldemort",
+  "photo":"https://img.buzzfeed.com/buzzfeed-static/static/2016-03/6/15/campaign_images/webdr08/how-lord-voldemort-are-you-g2wy-2-6896-1457296977-1_dblbig.jpg",
   "scores":[
       2,
-      5,
-      3,
       2,
-      4,
-      5,
-      1,
-      3,
       2,
-      1
+      2,
+      2,
+      2,
+      2,
+      2
     ]
 },
 {
-	"name":"sundar",
-  "photo":"http://s1.r29static.com//bin/entry/9be/x,80/1824829/image.jpg",
+	"name":"Agent Smith",
+  "photo":"https://thelycaeum.files.wordpress.com/2014/02/agent-smith.jpg",
   "scores":[
-      5,
-      4,
-      2,
       3,
+      3,
+      3,
+      3,
+      3,
+      1,
+      3,
+      3  
+    ]
+},
+{
+	"name":"Freddy Kreuger",
+  "photo":"https://vignette.wikia.nocookie.net/deathbattlefanon/images/b/b2/Freddy_Krueger.jpg/revision/latest?cb=20140814141144",
+  "scores":[
+      4,
+      4,
+      4,
+      4,
+      4,
       2,
       4,
-      5,
-      1,
-      2,
       4
     ]
 },
 {
-	"name":"satya",
-  "photo":"https://www.nerdmuch.com/wp-content/uploads/2016/07/arya-kills-cersei.jpg",
+	"name":"O Ren ishii",
+  "photo":"https://static.comicvine.com/uploads/original/11121/111214434/4415925-4011160928-894b1.jpg",
   "scores":[
-      3,
-      4,
-      2,
-      4,
-      3,
-      5,
       1,
       2,
-      5,
-      1
+      3,
+      4,
+      1,
+      2,
+      3,
+      4
     ]
 },
 {
-	"name":"shivam",
-  "photo":"https://img00.deviantart.net/db1c/i/2016/237/6/a/tyrion_lannister___peter_dinklage_by_tomatosoup13-daeahx5.jpg",
+  "name":"Hannibal Lecter",
+  "photo":"http://www.movies.ie/wp-content/uploads/2016/07/silence-of-the-lambs-anthony-hopkins-lecter2.jpg",
   "scores":[
-      2,
       4,
-      5,
-      3,
       1,
-      2,
       3,
+      2,
       4,
-      5,
-      2
+      2,
+      4,
+      3
+    ]
+},
+{
+  "name":"Joker",
+  "photo":"https://i.pinimg.com/736x/86/22/ae/8622ae3e39fbb2b2ebf9afa6b12befa5--dc-comic-comic-art.jpg",
+  "scores":[
+      4,
+      1,
+      4,
+      3,
+      2,
+      1,
+      1,
+      1
     ]
 }
 
